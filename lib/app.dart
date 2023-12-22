106d2821ed008566c3907b8b21ebbc8fb407b0ab
Chandram-Dutta <chandramdutta2004@gmail.com>
2023-12-22 20:08:39 +0530

    refactor: home page

--- lib/app.dart
+++ lib/app.dart
@@ -14,16 +14,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("GibSpons"),
-      ),
-    );
-  }
-}
